f60e180d4bea5a9c783f4282f5df19ec31854a08
Fernando Cagua <efcaguab@gmail.com>
2021-04-01 19:27:33 +1300

    Read input data

--- _targets.R
+++ _targets.R
@@ -1,6 +1,8 @@
 # Prepare workspace -------------------------------------------------------
 
+options(tidyverse.quiet = TRUE)
 library(targets)
+library(tidyverse)
 
 # load functions
 f <- lapply(list.files(path = here::here("R"), full.names = TRUE,
@@ -10,8 +12,26 @@ f <- lapply(list.files(path = here::here("R"), full.names = TRUE,
 
 list(
   tar_target(
-    name = dummy_target,
-    command = "hello-world"
+    name = asfis_list,
+    command = readr::read_csv(
+      file = "data/raw/ASFIS_sp_2020.txt",
+      col_types = readr::cols(.default = readr::col_character()))
+  ),
+  tar_target(
+    name = asfis_divisions,
+    command = readr::read_csv(
+      file = "data/raw/CL_FI_SPECIES_ISSCAAP_DIVISION.csv",
+      col_types = readr::cols(.default = readr::col_character()))
+  ),
+  tar_target(
+    name = ihh_list,
+    command = readr::read_csv(
+      file = "data/raw/IHH_species_list.csv",
+      col_types = readr::cols(.default = readr::col_character()))
+  ),
+  tar_target(
+    name = merged_list,
+    command = merge_tables(ihh_list, asfis_list, asfis_divisions)
   )
 )
 
